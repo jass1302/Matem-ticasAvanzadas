881d6917ea145b25fa1c25eb0b24f05a3aad548b
Hillrod <abam_espana@hotmail.com>
2019-05-27 14:18:13 -0500

    Serie de Fourier v2

---
+++ src/GUI/ScatterPlot.java
@@ -0,0 +1,52 @@
+
+package GUI;
+import Objetos.ParOrdenado;
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class ScatterPlot extends JFrame {
+  private static final long serialVersionUID = 6294689542092367723L;
+
+ 
+
+  public ScatterPlot(String title,ArrayList<ParOrdenado> arraa) {
+    super(title);
+    XYSeries series = new XYSeries("Fourier");
+    XYSeriesCollection dataset = new XYSeriesCollection();
+    // Create dataset
+   for(ParOrdenado O:arraa){
+    
+    series.add(O.x,O.y);
+    }
+    dataset.addSeries(series);
+    
+
+    // Create chart
+    JFreeChart chart = ChartFactory.createScatterPlot(
+        "Fourier", 
+        "X-Axis", "Y-Axis", dataset);
+
+    
+    //Changes background color
+    XYPlot plot = (XYPlot)chart.getPlot();
+    plot.setBackgroundPaint(new Color(255,228,196));
+    
+   
+    // Create Panel
+    ChartPanel panel = new ChartPanel(chart);
+    setContentPane(panel);
+  }
+}
+
---
+++ src/Herramientas/Fourier.java
@@ -0,0 +1,109 @@
+
+package Herramientas;
+import GUI.ScatterPlot;
+import Objetos.ParOrdenado;
+import java.util.ArrayList;
+import metodosIntegracion.Trapecio;
+
+public class Fourier{
+
+    private double limSum,limInf,limSup,dif;
+    private String funcion;
+    public Trapecio T;
+    public int div;
+    
+  
+      
+    public double getLimSum() {
+        return limSum;
+    }
+
+    public void setLimSum(double limSum) {
+        this.limSum = limSum;
+    }
+
+    public double getIntervInf() {
+        return limInf;
+    }
+
+    public void setIntervInf(double limInf) {
+        this.limInf = limInf;
+    }
+
+    public double getIntervSup() {
+        return limSup;
+    }
+
+    public void setIntervSup(double limSup) {
+        this.limSup = limSup;
+    }
+
+    public String getFuncion() {
+        return funcion;
+    }
+
+    public void setFuncion(String funcion) {
+        this.funcion = funcion;
+    }
+
+    public Fourier(double limSum, double limInf, double limSup, String funcion,int div) {
+        this.limSum = limSum;
+        this.limInf = limInf;
+        this.limSup = limSup;
+        this.funcion = funcion;
+        T = new Trapecio();
+        dif = limSup-limInf;
+        this.div = div;
+      
+    }
+    
+    public double integral(){
+        return Double.parseDouble(T.metodoT(limInf+"", limSup+"",funcion,div+""));
+    }
+    
+    public double aN(int n){
+        return Double.parseDouble(T.metodoT(limInf+"", limSup+"","("+funcion+")*cos("+n+"*pi*x/"+limSup+")",div+""));
+    }
+    
+    public double bN(int n){
+        return Double.parseDouble(T.metodoT(limInf+"", limSup+"","("+funcion+")*sin("+n+"*pi*x/"+limSup+")",div+""));
+    }
+    
+    public double evaluateFX(double xValue){
+        double fx = integral()/2;
+        for (int i = 1; i < limSum; i++) {
+            fx+=(aN(i)*Math.cos(i*Math.PI*xValue/limSup))+(bN(i)*Math.sin(i*Math.PI*xValue/limSup));
+        }
+        return fx;
+    }
+    
+    public String coeficientes(){
+        String coef;
+        double fx = integral()/2;
+        coef = "a0 = "+fx+"\n";
+        for (int i = 1; i < limSum; i++) {
+            coef += "a"+i+" = "+aN(i)+"   b"+i+" = "+bN(i)+"\n";
+        }
+        return coef;
+    }
+    
+    public void pruebaFX(double h,int div){
+        double lim= limInf;
+        for (int i = 0; i <= h; i++) {
+            System.out.println(lim+"\t"+evaluateFX(lim));
+            lim+=dif/h;
+        }
+    }
+    
+    public ArrayList<ParOrdenado> createGraph(double h){
+        ArrayList<ParOrdenado> array = new ArrayList<>();
+        ParOrdenado xd;
+        double lim= limInf;
+        for (int i = 0; i <= h; i++) {
+            xd = new ParOrdenado(lim,evaluateFX(lim));
+            array.add(xd);
+            lim+=dif/h;
+        }
+        return array;
+    }
+}
